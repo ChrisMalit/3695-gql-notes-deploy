15cc434a7d2426e144632598f23aebac8882da73
ChrisMalit <49224820+ChrisMalit@users.noreply.github.com>
2020-11-18 18:10:19 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,39 +2,28 @@ const ApolloServer = require('apollo-server').ApolloServer
 const ApolloServerLambda = require('apollo-server-lambda').ApolloServer
 const { gql } = require('apollo-server-lambda');
 
-const mongoose = require('mongoose');
-const cron = require('node-cron').ApolloServer;
-const moment = require('moment').ApolloServer;
-var cloudinary = require('cloudinary').v2;
-
-mongoose.connect('mongodb+srv://admin:P@ssw0rd@cluster0.zo5ak.mongodb.net/<dbname>?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-var mongooseConnect = mongoose.connection;
-
-
 const typeDefs = gql`
-  type Query {
-    hello: String
-  }
+    type Query {
+        hello: String
+    }
 `;
 
 const resolvers = {
-  Query: {
-    hello: () => "Hi! Love from @stemmlerjs 🤠."
-  }
+    Query: {
+        hello: () => "Hi! Love from @stemmlerjs 🤠."
+    }
 };
+
 function createLambdaServer () {
     return new ApolloServerLambda({
-        typeDefs,
-        resolvers,
-        introspection: true,
-        playground: true,
+      typeDefs,
+      resolvers,
+      introspection: true,
+      playground: true,
     });
-}
+  }
   
-  function createLocalServer () {
+function createLocalServer () {
     return new ApolloServer({
         typeDefs,
         resolvers,
