a14e4e536909313f29d2c76288cc7e3cf14eff37
ChrisMalit <49224820+ChrisMalit@users.noreply.github.com>
2020-11-18 17:57:33 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ const ApolloServer = require('apollo-server').ApolloServer
 const ApolloServerLambda = require('apollo-server-lambda').ApolloServer
 const { gql } = require('apollo-server-lambda');
 
-const mongoose = require('mongoose').ApolloServer;
+const mongoose = require('mongoose');
 const cron = require('node-cron').ApolloServer;
 const moment = require('moment').ApolloServer;
 var cloudinary = require('cloudinary').v2;
+
 const typeDefs = gql`
   type Query {
     hello: String
